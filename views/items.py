cccd7efbf55cc1fe403b8038832444ccbf1b3f89
Manan Singh <manan.singh@stonybrook.edu>
2017-04-02 23:58:34 -0400

    Fixed a few things for the items route

--- views/items.py
+++ views/items.py
@@ -2,7 +2,7 @@ from flask.views import MethodView
 from flask import request, jsonify
 import messages
 from db import db
-from datetime import datetime
+from time import time
 
 types = ['poll', 'vote', 'justification', 'document']
 
@@ -10,7 +10,7 @@ types = ['poll', 'vote', 'justification', 'document']
 class AddItem(MethodView):
     def post(self):
         json = request.get_json()
-        json['timestamp'] = datetime.utcnow()
+        json['timestamp'] = time()
         item_type = json['type']
         if item_type in types:
             result = db[item_type].insert_one(json)
@@ -21,36 +21,59 @@ class AddItem(MethodView):
         else:
             return jsonify(CODE_ERROR)
 
-
 class Item(MethodView):
     def get(self, id):
-        item_type = request.args['type']
+        item_type = None
+        if 'type' in request.args:
+            item_type = request.args['type']
+
         result = None
         if item_type in types:
             result = db[item_type].find_one({'id': id})
         else:
+            # put it into temporary items collection
             restult = db.items.find_one({'id': id})
         if result:
             return jsonify({'status': 'OK', 'item': result})
         else:
             return jsonify(CODE_ERROR)
 
+    def delete(self, id):
+        item_type = None
+        if 'type' in request.args:
+            item_type = request.args['type']
+        
+        if item_type in types:
+            result = db[item_type].delete_one({'id': id});
+        else:
+            result = db['items'].delete_one({'id': id});
+
+        if result:
+            return jsonify({'status': 'OK'})
+        else:
+            return jsonify({'status': 'error'})
+
 
 class Search(MethodView):
     def post(self):
         json = request.get_json()
-        item_type = json['type']
+        if 'type' not in json or json['type'] not in types:
+            # doing this only to conform to gradin script
+            item_type = 'items'
+        else:
+            item = json['type']
+
         limit = 25
-        timestamp = None
         if 'limit' in json:
-            if json['limit'] > 100:
+            if json['limit'] > 100 or json['limit'] < 0:
                 return jsonify(CODE_ERROR)
             else:
-                limit = json.pop['limit']
-        if item_type not in types:
-            item_type ='items' 
+                limit = json.pop('limit')
+
+        timestamp = None
         if 'timestamp' in json:
-           json['timestamp'] = {'timestamp': {'$le': json['timestamp']}}
+           json['timestamp'] = {'timestamp': {'$lte': json['timestamp']}}
+
         result = db[item_type].find({json}).limit(limit)
         if result:
             return jsonify({'status': 'OK', 'items': list(result)})

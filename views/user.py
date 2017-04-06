90017b9dfd875437d5f56742b9ca12a57d7e2f89
Ubuntu <ubuntu@loadbalancer.cloud.compas.cs.stonybrook.edu>
2017-04-06 16:18:41 +0000

    typo

--- views/user.py
+++ views/user.py
@@ -35,9 +35,9 @@ class Follow(MethodView):
             follow = json.pop('follow')
         if follow:
             db.user.update_one({'username':session['username']},{ '$push':{'followers':json['username']}})
-            db.user.update_one({'username':json['username']},{'&push':{'followers':session['username']}})
+            db.user.update_one({'username':json['username']},{'$push':{'followers':session['username']}})
         else:
             db.user.update_one({'username':session['username']},{ '$pull':{'followers':json['username']}})
-            db.user.update_one({'username':json['username']},{'&pull':{'followers':session['username']}})
+            db.user.update_one({'username':json['username']},{'$pull':{'followers':session['username']}})
 
         return jsonify(CODE_OK)

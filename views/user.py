0ac03b2f24f6b72aa118f23c3344b8feb0b4a6f4
Manan Singh <manan.singh@stonybrook.edu>
2017-04-06 12:30:19 -0400

    Fixed a bug in follow

--- views/user.py
+++ views/user.py
@@ -34,10 +34,10 @@ class Follow(MethodView):
         if 'follow' in json:
             follow = json.pop('follow')
         if follow:
-            db.user.update_one({'username':session['username']},{ '$push':{'followers':json['username']}})
+            db.user.update_one({'username':session['username']},{ '$push':{'following':json['username']}})
             db.user.update_one({'username':json['username']},{'$push':{'followers':session['username']}})
         else:
-            db.user.update_one({'username':session['username']},{ '$pull':{'followers':json['username']}})
+            db.user.update_one({'username':session['username']},{ '$pull':{'following':json['username']}})
             db.user.update_one({'username':json['username']},{'$pull':{'followers':session['username']}})
 
         return jsonify(CODE_OK)
